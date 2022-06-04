af0a55f433d59938fbeb4646afeb0e565f085b9a
JamesQian123 <jamesqian12345678910@gmail.com>
2022-06-04 12:49:10 -0700

    a

---
+++ src/_01_algorithms/_1_prime_or_not/prime_or_not.java
@@ -0,0 +1,59 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+
+public class prime_or_not {
+	public static void main(String[] args) {
+		String number=JOptionPane.showInputDialog(null, "type a number");
+		int number1=Integer.parseInt(number);
+		for ( int i = 2; i < number1; i++){
+		if (number1 % i == 0) {
+			JOptionPane.showMessageDialog(null, number1+"isn't prime");
+			
+		}
+		
+		}
+	
+	
+	
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+	private static void parseint(String number) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
