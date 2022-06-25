e095faf91cdc002b7a8f3f1b694d7323d9369a22
league <league@imac-24.attlocal.net>
2022-06-25 13:01:43 -0700

    s

--- src/_03_method_writing/_1_obedient_robot/rtpbpt.java
+++ src/_03_method_writing/_1_obedient_robot/rtpbpt.java
@@ -1,25 +1,69 @@
 package _03_method_writing._1_obedient_robot;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class rtpbpt {
-	static Robot myRobot=new Robot();
-public static void main(String[] args) {
-drawSquare();
+	static Robot myRobot = new Robot();
 
+	public static void main(String[] args) {
+		String answer = JOptionPane.showInputDialog(null, "WHAT SHAPE WOULD U LIKE THE ROBOT TO DRAWWWWWWWW!");
+		if (answer.equals("drawSquare")) {
+			drawSquare();
+		} else {
+			if (answer.equals("drawTriangle")) {
+				drawTriangle();
+			}
+			else {
+				if(answer.equals("drawCircle")) {
+					drawCircle();
+				}
+			
+			}
+		}
 
+	}
+
+	
+
+	public static void drawSquare() {
+		System.out.println("drawSquare");
+		myRobot.penDown();
+		myRobot.move(50);
+		myRobot.turn(90);
+		myRobot.move(50);
+		myRobot.turn(90);
+		myRobot.move(50);
+		myRobot.turn(90);
+		myRobot.move(50);
+
+	}
+
+	public static void drawTriangle() {
+		System.out.println("drawTriangle");
+		myRobot.penDown();
+		myRobot.turn(120);
+		myRobot.move(100);
+		myRobot.turn(120);
+		myRobot.move(100);
+		myRobot.turn(120);
+		myRobot.move(100);
+	}
+
+public static void drawCircle() {
+ myRobot.penDown();
+for ( int i = 0; i < 360; i++){
+	myRobot.turn(1);
+	myRobot.move(50);
 }
+	
 
-public static void drawSquare() {
-	System.out.println("drawSquare");
-	myRobot.penDown();
-	myRobot.move(50);
-	myRobot.turn(90);
-	myRobot.move(50);
-	myRobot.turn(90);
-	myRobot.move(50);
-	myRobot.turn(90);
-	myRobot.move(50);
+		
+	
+
+
+
 
 }
 }
